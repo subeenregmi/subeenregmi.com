52eb0c5e9ed1920bee8f6bb3b4adda1ff88499b7
Subeen Regmi <subeenregmi123@gmail.com>
2025-06-14 01:51:04 +0100

    feat(blogs): added getting the md for a blog

---
+++ src/service/blog_service.go
@@ -0,0 +1,49 @@
+package blog_service
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	//"github.com/gomarkdown/markdown"
+)
+
+func GetBlogRoot() *os.Root {
+	root, err := os.OpenRoot("../public/blogs")
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Blog folder cannot be found: %e", err))
+	}
+	return root
+}
+
+func BlogExists(root *os.Root, blog string) bool {
+	dirinfo, err := root.Open(blog)
+	defer dirinfo.Close()
+
+	return err == nil
+}
+
+func GetBlogMD(root *os.Root, blog string) string {
+	file, err := root.Open(blog + "/main.md")
+	if err != nil {
+		log.Printf("MD for %v has not been found: %e", blog, err)
+		return ""
+	}
+
+	buffer := make([]byte, 100)
+	
+	var contentBuilder strings.Builder
+
+	rb := 1
+
+	for rb != 0 {
+		rb, err := file.Read(buffer)
+		if err != nil || rb == 0 {
+			break
+		}
+		contentBuilder.Write(buffer)
+	}
+
+	return contentBuilder.String()
+}
