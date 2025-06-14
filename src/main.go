2323ac58db715a990b774fc94b594af55813ad33
Subeen Regmi <subeenregmi123@gmail.com>
2025-06-14 11:27:20 +0100

    refactor(router): changed the router in main to use router specified in api layer

--- src/main.go
+++ src/main.go
@@ -1,23 +1,11 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"github.com/subeenregmi/subeenregmi.com/api"
 )
 
 func main () {
-	var router *gin.Engine
-	router = gin.Default()
-
-	router.LoadHTMLGlob("../public/*.html")
-
-	router.Static("/static", "../public/static")
-
-	router.GET("/", api.RootHandler)	
-	router.GET("/blogs", api.BlogsHandler)
-	router.GET("/blogs/:blog", api.BlogsHandler)
-	router.GET("/projects", api.ProjectsHandler)
-	router.GET("/photos", api.PhotosHandler)
+	router := api.Router()
 
 	router.Run("0.0.0.0:8080")
 }
