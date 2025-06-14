6417595a2c30b0c80c4c0cf6b77f8ed85b120eb3
Subeen Regmi <subeenregmi123@gmail.com>
2025-06-14 11:26:10 +0100

    feat(blog): modified api router to use the blog service layer

--- src/api/api.go
+++ src/api/api.go
@@ -1,9 +1,11 @@
 package api
 
 import (
-	"os"
-	"log"
+	"html/template"
 	"net/http"
+
+	"github.com/subeenregmi/subeenregmi.com/service"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -17,32 +19,52 @@ func BlogsHandler(c *gin.Context) {
 
 func BlogHandler(c *gin.Context) {
 	blog := c.Param("blog")
-
-	_, err := os.Stat("../public/blogs" + blog)
 	
-	if err != nil {
-		log.Fatal(err)
+	if blog == "" {
+		NoRouteHandler(c)
+		return
 	}
 
-	c.HTML(
-		http.StatusOK,
-		"../public/blogs" + blog + "/main.html",
-		gin.H{},
-	)
+	html := blog_service.GetBlog(blog)
+
+	c.HTML(http.StatusOK, "template.html", gin.H{
+		"title": blog,
+		"back_url": "/blogs",
+		"page_content": template.HTML(html),
+	})	
 }
 
 func ProjectsHandler(c *gin.Context) {
-	c.HTML(
-		http.StatusOK,
-		"projects.html",
-		gin.H{},
-	)
+	c.HTML(http.StatusOK, "projects.html",gin.H{})
 }
 
 func PhotosHandler(c *gin.Context) {
-	c.HTML(
-		http.StatusOK,
-		"photos.html",
-		gin.H{},
-	)
+	c.HTML(http.StatusOK, "photos.html", gin.H{})
 }
+
+func NoRouteHandler(c *gin.Context) {
+	c.HTML(http.StatusNotFound, "template.html", gin.H{
+		"title": "Page Missing",
+		"back_url": "/",
+		"page_content": "The page you are trying to access is missing. :(",
+	})
+}
+
+func Router() *gin.Engine {
+	var router *gin.Engine
+	router = gin.Default()
+
+	router.LoadHTMLGlob("../public/*.html")
+
+	router.Static("/static", "../public/static")
+	router.GET("/", RootHandler)
+	router.GET("/blogs", BlogsHandler)
+	router.GET("/blogs/:blog", BlogHandler)
+	router.GET("/projects", ProjectsHandler)
+	router.GET("/photos", PhotosHandler)
+
+	router.NoRoute(NoRouteHandler)
+	
+	return router
+}
+
