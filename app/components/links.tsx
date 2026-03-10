106eeb6d532bb8d74228478a7d04153c23e5279f
Subeen Regmi <subeenregmi123@gmail.com>
2026-03-10 17:47:51 +0000

    feat: use Link and viewTransition

--- app/components/links.tsx
+++ app/components/links.tsx
@@ -1,42 +1,44 @@
+import { Link } from "react-router";
 import { cn } from "@/utils";
 import Icon from "./icon";
 
 export interface Link {
-  content: any;
-  url: string;
-  external?: boolean;
-  externalIcon?: boolean;
+	content: any;
+	url: string;
+	external?: boolean;
+	externalIcon?: boolean;
 }
 
 export interface LinksProps {
-  links?: Link[];
-  className?: string;
+	links?: Link[];
+	className?: string;
 }
 
 export default function Links({ links, className }: LinksProps) {
-  return links?.map(({ content, url, external, externalIcon }, i) => {
-    return (
-      <a
-        key={i}
-        className={cn(
-          "text-swhite-25",
-          "underline",
-          "underline-offset-[2.5px]",
-          "transition-colors",
-          "duration-150",
-          "ease-linear",
-          "hover:text-swhite-75",
-          "cursor-pointer",
-          className,
-        )}
-        href={url}
-        target={external ? "_blank" : "_self"}
-      >
-        {content}
-        {externalIcon && (
-          <Icon className="text-sm ml-1" variant="externalLink" />
-        )}
-      </a>
-    );
-  });
+	return links?.map(({ content, url, external, externalIcon }, i) => {
+		return (
+			<Link
+				key={i}
+				className={cn(
+					"text-swhite-25",
+					"underline",
+					"underline-offset-[2.5px]",
+					"transition-colors",
+					"duration-150",
+					"ease-linear",
+					"hover:text-swhite-75",
+					"cursor-pointer",
+					className,
+				)}
+				to={url}
+				viewTransition={!external}
+				target={external ? "_blank" : "_self"}
+			>
+				{content}
+				{externalIcon && (
+					<Icon className="text-sm ml-1" variant="externalLink" />
+				)}
+			</Link>
+		);
+	});
 }
