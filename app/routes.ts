5e747979e45413347944cbeb3bc0be509f476170
Subeen Regmi <subeenregmi123@gmail.com>
2026-09-07 10:18:37 +0100

    feat: robots.txt

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,6 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import { index, type RouteConfig, route } from "@react-router/dev/routes";
 
 export default [
-  index("index.tsx"),
-  route("whoami", "routes/whoami/whoami.tsx"),
+	index("index.tsx"),
+	route("whoami", "routes/whoami/whoami.tsx"),
 ] satisfies RouteConfig;
