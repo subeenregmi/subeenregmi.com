53fbeb0aa61387f9c7f58b249130ba2f2ae7fd21
Subeen Regmi <subeenregmi123@gmail.com>
2026-09-16 18:51:03 +0100

    vite updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+	plugins: [tailwindcss(), reactRouter()],
+	resolve: {
+		tsconfigPaths: true,
+	},
 });
